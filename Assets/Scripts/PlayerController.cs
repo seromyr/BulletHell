ba25ac5259205802cc4b384e9d488d85a6978649
seromyr <55792226+seromyr@users.noreply.github.com>
2020-10-19 22:32:13 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick joystick;
+
+    [SerializeField]
+    private Transform playerPointer;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float speedMultiplier;
+
+    void Start()
+    {
+        joystick = GameObject.Find("Fixed Joystick").GetComponent<Joystick>();
+    }
+
+    private void FixedUpdate()
+    {
+        Controller();
+    }
+
+    private void Controller()
+    {
+        speed = new Vector2(joystick.Horizontal, joystick.Vertical).magnitude * speedMultiplier;
+
+        if (joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical > 0 || joystick.Vertical < 0)
+        {
+            playerPointer.position = new Vector3(joystick.Horizontal + transform.position.x, playerPointer.position.y, joystick.Vertical + transform.position.z);
+
+            transform.LookAt(new Vector3(playerPointer.position.x, 0, playerPointer.position.z));
+
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject destroyableBullet;
+
+    private float time;
+    void Start()
+    {
+        destroyableBullet = Resources.Load<GameObject>("Prefabs/KillAbleBullet");
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= time + 0.1f)
+        {
+            var meNew = Instantiate(destroyableBullet, transform.position + transform.forward * 2, transform.rotation);
+            meNew.GetComponent<SelfMovingFoward>().direction = transform.position;
+            time = Time.time;
+        }
+        //meNew.transform.parent = null;
+    }
+
+
+}
